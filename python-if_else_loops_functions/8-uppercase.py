36d19f51f8e234daf6834de7cf4a5f2850f80f6e
Meashyshy <camillefvrl@gmail.com>
2023-01-19 05:51:40 -0800

    Added uppercase variable

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("{}".format(str.upper()))
+    for i in str:
+        if (ord(i) >= 97 and ord(i) <= 122):
+            letter_uppercase = ord(i) - 32
+            print("{}".format(chr(letter_uppercase)), end="")
+        else:
+            print("{}".format(i), end ="")
