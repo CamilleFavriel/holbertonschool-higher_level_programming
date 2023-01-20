34f5c1bdc40e6afbd4fc490d17d46773e1e81e4d
Meashyshy <camillefvrl@gmail.com>
2023-01-20 09:37:30 -0800

    Added blank lines

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def main():
     from calculator_1 import add, sub, mul, div
+    
     a = 10
     b = 5
+    
     add_result = add(a, b)
     sub_result = sub(a, b)
     mul_result = mul(a, b)
