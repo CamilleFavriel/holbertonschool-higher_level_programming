df7dd245d95c38d3ef338bcfc91d21c0f1c99681
Meashyshy <camillefvrl@gmail.com>
2023-01-20 09:55:12 -0800

    Deleted end of file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -8,7 +8,3 @@ def main():
     print("{} + {} = {}".format(a, b, sub(a, b)))
     print("{} + {} = {}".format(a, b, mul(a, b)))
     print("{} + {} = {}".format(a, b, div(a, b)))
-
-
-if __name__ == "__main__":
-    main()
