902e54dbf02c70a9de7dcb024eb49600ee6fec9d
Meashyshy <camillefvrl@gmail.com>
2023-01-21 09:24:22 -0800

    PEP8 validation

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
     argv = sys.argv[1:]
-    
+ 
     if len(argv) == 0:
         print("0 arguments.")
-    
-    
+
     elif len(argv) == 1:
         print("1 argument:")
 
-    
     else:
         print(f"{len(argv)} arguments:")
 
-    
     for i, words in enumerate(argv, start=1):
-    # Print the position and value of each argument
         print(f"{i}: {words}")
 
 
 if __name__ == "__main__":
-        main()
+    main()
