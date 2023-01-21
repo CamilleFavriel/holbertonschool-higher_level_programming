d35da324f1da8edb092275150677a7b4fc30ffcd
Meashyshy <camillefvrl@gmail.com>
2023-01-21 09:15:03 -0800

    Added file

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+argv = argv[1:]
+
+if len(argv) == 0:
+    print("0 arguments.")
+
+elif len(argv) == 1:
+    print("1 argument:")
+
+else:
+    print(f"{len(argv)} arguments:")
+
+for i, words in enumerate(argv, start=1):
+    # Print the position and value of each argument
+    print(f"{i}: {words}")
