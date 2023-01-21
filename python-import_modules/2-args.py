5720ef6e899f03397aa60ab5ea3720a35b180f73
Meashyshy <camillefvrl@gmail.com>
2023-01-21 09:21:54 -0800

    Defined main function

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 import sys
 
-argv = sys.argv[1:]
+def main():
+    argv = sys.argv[1:]
+    
+    if len(argv) == 0:
+        print("0 arguments.")
+    
+    
+    elif len(argv) == 1:
+        print("1 argument:")
 
-if len(argv) == 0:
-    print("0 arguments.")
+    
+    else:
+        print(f"{len(argv)} arguments:")
 
-elif len(argv) == 1:
-    print("1 argument:")
-
-else:
-    print(f"{len(argv)} arguments:")
-
-for i, words in enumerate(argv, start=1):
+    
+    for i, words in enumerate(argv, start=1):
     # Print the position and value of each argument
-    print(f"{i}: {words}")
+        print(f"{i}: {words}")
 
 
 if __name__ == "__main__":
-    main()
+        main()
