3ee1c48df55a519c5d71b697d0f84b0190333421
Meashyshy <camillefvrl@gmail.com>
2023-01-25 08:51:07 -0800

    Variable fixed

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,5 +5,5 @@ def replace_in_list(my_list, idx, element):
     
     for i in range(len(my_list)):
 
-        if my_list[i] == my_list[3]:
-            my_list[3] = element
+        if my_list[i] == my_list[:3]:
+            my_list[i] = element
