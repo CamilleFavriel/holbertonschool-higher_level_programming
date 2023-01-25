96d921340203059565df450e9bb0d933b3d56652
Meashyshy <camillefvrl@gmail.com>
2023-01-25 09:01:32 -0800

    Fixed return

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,10 +2,9 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    
-    for i in range(len(my_list)):
 
+    for i in range(len(my_list)):
         if my_list[i] == my_list[idx]:
             my_list[idx] = element
 
-            return my_list[i]
+    return my_list
