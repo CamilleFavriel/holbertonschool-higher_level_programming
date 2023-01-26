9e22ac7d16fe5aebb66ba9df13491a46e2fd2142
Meashyshy <camillefvrl@gmail.com>
2023-01-26 04:09:15 -0800

    Cameback to old-version

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for lines in matrix:
         for val in lines:
             print("{:d}".format(val), end="")
-        print("\n", end=" ")
+        print()
