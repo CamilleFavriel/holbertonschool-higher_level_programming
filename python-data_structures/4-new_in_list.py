a6313bfeca16b24ff19b36ecc7163cf69bcfdae8
Meashyshy <camillefvrl@gmail.com>
2023-01-26 02:45:10 -0800

    Added file

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,9 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    
-    for i in range(len(my_list)):
-        if my_list[i] == my_list[idx]:
-            my_list[idx] = element
-
-    return my_list
+    else:
+        new_list = my_list
+        new_list[idx] = element
+        return new_list
