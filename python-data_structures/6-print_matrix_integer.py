bd5107515428ab1eaee29a56c25effbf98827f64
Meashyshy <camillefvrl@gmail.com>
2023-01-26 04:20:57 -0800

    Added return in case matrix is empty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+    
     for lines in matrix:
         for i, val in enumerate(lines):
                 print("{:d}".format(val), end=" " if i < len(lines) - 1 else "\n")
