a0eea8f09f14f20f989ab65289bd5c312467c0df
Meashyshy <camillefvrl@gmail.com>
2023-01-26 06:37:09 -0800

    Integrated sum and zip functions

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2] if len(tuple_a) >= 2 else (0, 0)
-    b1, b2 = tuple_b[:2] if len(tuple_b) >= 2 else (0, 0)
-    return (a1 + b1, a2 + b2)
+    tuple_a = tuple_a[:2] + (0, 0)[len(tuple_a):2]
+    tuple_b = tuple_b[:2] + (0, 0)[len(tuple_b):2]
+    return tuple(map(sum, zip(tuple_a, tuple_b)))
