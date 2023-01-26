389d723c34e82788d33fc0617f51adea1c8a9d4c
Meashyshy <camillefvrl@gmail.com>
2023-01-26 04:14:31 -0800

    Added enumerate function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for lines in matrix:
-        for i, val in lines:
+        for i, val in enumerate(lines):
             if i < len(lines) - 1:
                 print("{:d}".format(val), end=" ")
         print()
