ebc790ab9b3630949f5d69535ecb119e61585c2e
Meashyshy <camillefvrl@gmail.com>
2023-01-27 08:09:07 -0800

    Added file

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     for i in my_list:
-        if search != 0:
-            search = replace
-            return my_list
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
