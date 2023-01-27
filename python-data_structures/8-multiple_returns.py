2b06483a6443d73cfdf72e3e941d15269eb78c58
Meashyshy <camillefvrl@gmail.com>
2023-01-27 06:56:37 -0800

    Fixed syntax errro

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if not sentence:
         sentence[:1] == None
     else:
-        return (len(sentence), sentence[O])
+        return (len(sentence), sentence[0])
