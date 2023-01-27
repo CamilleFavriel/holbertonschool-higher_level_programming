266623cb175ab4b68c5c25f79809cb713b43fd53
Meashyshy <camillefvrl@gmail.com>
2023-01-27 08:36:15 -0800

    Added file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in my_list:
-        if count == x:
-            return
-        print(i)
-        count += 1
+    try:
+        print(my_list[x])
+    except KeyError:
+        print(None)
