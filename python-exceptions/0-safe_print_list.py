9f1945b565733d78703945f0c2dfcb7c4ba69334
Meashyshy <camillefvrl@gmail.com>
2023-01-27 09:16:38 -0800

    Deleted space

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
-        print()
-        return x
-    except:
-        for i in range(x):
-            try:
-                print(my_list[i], end="")
-            except IndexError:
-                break
-            print()
-            return i
+            print(my_list[i], end='')
+    except IndexError:
+        pass
+    print()
+    return i
