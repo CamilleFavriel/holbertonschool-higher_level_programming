b97bbdb38a51781aed0f69bb32b5a9bcad6dd6c6
Meashyshy <camillefvrl@gmail.com>
2023-01-30 07:00:43 -0800

    Added second parameter to pop

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, keys=""):
-    if keys in a_dictionary is None:
-        return a_dictionary
-    a_dictionary.pop(keys)
+    a_dictionary.pop(keys, a_dictionary)
     return a_dictionary
