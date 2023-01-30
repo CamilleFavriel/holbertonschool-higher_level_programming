717e0544109860fb0b905f2794e7b98191449564
Meashyshy <camillefvrl@gmail.com>
2023-01-30 07:24:50 -0800

    PEP8 Validation

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for k, v in a_dictionary.items():
-# When looping through dictionaries, the key and corresponding 
-# value can be retrieved at the same time using the items() method.
+    # When looping through dictionaries, the key and corresponding
+    # value can be retrieved at the same time using the items() method.
         new_dict[k] = v * 2
     return new_dict
