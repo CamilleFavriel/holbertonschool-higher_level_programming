8bccc42a3df4a6aa2b12588e22a8981eb68bafbe
Meashyshy <camillefvrl@gmail.com>
2023-02-02 02:59:36 -0800

    Fixed documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,9 +4,8 @@
 """
 
 class Square:
+    """
+    A class representing a square.
+    """
+
     object = "{}"
-    
-    """
-    Attributes:
-    object (str): The string representation of the square.
-    """
