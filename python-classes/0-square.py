e5069654ad7ad3087275059614e2265652aa5b9f
Meashyshy <camillefvrl@gmail.com>
2023-02-02 02:38:11 -0800

    Fixed documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 class Square:
     """
-    Square class has a single class attribute, `object`, which is a string representation of a square.
+    A class representing a square.
+
+    Attributes:
+        object (str): The string representation of the square.
     """
+
     object = "{}"
