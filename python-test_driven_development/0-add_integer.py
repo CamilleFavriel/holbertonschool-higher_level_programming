36ba1c3ec2eecae17ea699a629f34c328b222f27
Meashyshy <camillefvrl@gmail.com>
2023-02-08 09:48:40 -0800

    Added files

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    try:
+        if isinstance(int(a) and int(b), float or int):
+            return (a, b)
+    except TypeError:
+        print("{} must be an integer", a)
+        print("{} must be an integer", b)       
