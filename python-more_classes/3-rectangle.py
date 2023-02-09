907feabb18205b0c7ce679819d8fb23643ff0e81
Meashyshy <camillefvrl@gmail.com>
2023-02-09 07:27:02 -0800

    PEP8 Validation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
             print("")
         else:
             return 2 * (self.width + self.height)
- 
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ''
