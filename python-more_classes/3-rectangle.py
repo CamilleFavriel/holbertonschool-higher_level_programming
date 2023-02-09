28462cbedeb035aa042fbd47b58512a39b150035
Meashyshy <camillefvrl@gmail.com>
2023-02-09 07:15:07 -0800

    Added file

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""Class that defines a rectangle based on 0-rectangle.py"""
+
+
+class Rectangle:
+    """Define a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height*self.width
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            print("")
+        else:
+            return 2 * (self.width + self.height)
