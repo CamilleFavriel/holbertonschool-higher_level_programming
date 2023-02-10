fa1a8df6e668d123dc1466d5b72a97a5b21ca7c2
Meashyshy <camillefvrl@gmail.com>
2023-02-10 13:46:16 -0800

    PEP8 Validation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Method that reads a text file"""
+
+
 def read_file(filename=""):
+    """ Reads a text file"""
     with open(filename, 'r', encoding='utf8') as f:
         print(f.read(), end='')
