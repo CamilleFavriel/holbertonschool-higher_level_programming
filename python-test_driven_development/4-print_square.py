2ff102b8a2a5345a255bfb372566211194522be6
Meashyshy <camillefvrl@gmail.com>
2023-02-13 13:06:52 -0800

    Added file

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Function that prints a square with the charcter #"""
+
+
+def print_square(size):
+    """Return square"""
+    if type(size) != int:
+        if type(size) == float and size < 0:
+            raise TypeError("size must be an integer")
+        else:
+            raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
