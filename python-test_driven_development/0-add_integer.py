fc0754840cee0ca1fc294047da0411fffb4e8470
Meashyshy <camillefvrl@gmail.com>
2023-02-13 11:32:07 -0800

    Added files

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-import doctest
 def add_integer(a, b=98):
-    """
-    Adds two integers together
-    a and b must be integers or floats, otherwise raise a TypeError exception with the message
-    'a must be an integer' or 'b must be an integer'
-    a and b must be casted to integers if they are float
-    """
     if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer or a float")
     if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer or a float")
     a = int(a)
     b = int(b)
     return a + b
-
-if __name__ == "__main__":
-    doctest.testmod()
