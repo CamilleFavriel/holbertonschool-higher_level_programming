a75500f2e8876e8ce756dc26e6106f8e818f4559
Meashyshy <camillefvrl@gmail.com>
2023-02-13 09:25:01 -0800

    Added files

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,9 +15,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-        return self.__size ** 2
-
+    
     @property
     def size(self):
         return self.__size
@@ -29,3 +27,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    
+    def area(self):
+        return self.__size ** 2
--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    i = 0
     try:
         print("{:d}".format(int(value)))
         return True
