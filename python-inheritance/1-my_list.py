04106ae1cdaaf443212230b0055596f92fda53d4
Meashyshy <camillefvrl@gmail.com>
2023-02-16 04:18:11 -0800

    Added files

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Function that returns the list of available attributes
+and methods of an object"""
+
+
+class MyList(list):
+ 
+    def print_sorted(self):
+        """Printsin ascending sort"""
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
