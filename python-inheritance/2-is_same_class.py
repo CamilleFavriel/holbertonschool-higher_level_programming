274adad9af0e18bf677f24190ef27cff6c607a36
Meashyshy <camillefvrl@gmail.com>
2023-02-19 11:16:03 -0800

    Added test_case

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,7 +6,8 @@ instance of the specified class; otherwise False."""
 
 def is_same_class(obj, a_class):
     """
-    Returns True if the object is exactly an
-    instance of the specified class; otherwise False.
+    Returns True if the object is an instance of, or if the object is
+    an instance of a class that inherited from, the specified class;
+    otherwise returns False.
     """
-    return type(obj) is a_class
+    return isinstance(obj, a_class)
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,10 +5,9 @@ or if the object is an instance of a
 class that inherited from, the specified class ; otherwise False"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
+    """Check if the object is an
+    instance of a class that inherited (directly or indirectly)
+    from the specified class.
     """
-    returns True if the object is an instance of,
-    or if the object is an instance of a
-    class that inherited from, the specified class ; otherwise False
-    """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
