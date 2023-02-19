30dd71185e34a3ecc954d17b9730a83438f60cad
Meashyshy <camillefvrl@gmail.com>
2023-02-19 15:18:05 -0800

    PEP8 Validation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a Rectangle class that inherits from the BaseGeometry class.
+This module defines a Rectangle class
+that inherits from the BaseGeometry class.
 """
 
 
