8be2c7a39508e348abf20162b7a62fcd06a7c791
Meashyshy <camillefvrl@gmail.com>
2023-02-20 01:07:55 -0800

    Added file

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Method that appends a string at the end of a text file"""
+
+
+def to_json_string(my_obj):
+    """Returns the JSOn representation of an object (string)"""
+    return json.dumps(my_obj)
