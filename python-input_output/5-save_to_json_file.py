306819a7b82f27434012f75ddfa1bffe5b117b6f
Meashyshy <camillefvrl@gmail.com>
2023-02-20 01:18:41 -0800

    Added file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Method that appends a string at the end of a text file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Returns the JSOn representation of an object (string)"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(my_obj)
+    return json.dumps(my_obj)
