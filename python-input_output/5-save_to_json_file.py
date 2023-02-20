87d003064ba57e263375346d59475d075a866eed
Meashyshy <camillefvrl@gmail.com>
2023-02-20 02:09:49 -0800

    Small changes

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Returns the JSOn representation of an object (string)"""
     with open("filename", "w", encoding="utf-8") as f:
-        return json.dump(my_obj, f)
+        return json.dump(my_obj, f, ensure_ascii=True)
