23e9533f471d3f88b0d1bef19b61300b12436974
Meashyshy <camillefvrl@gmail.com>
2023-02-21 01:29:09 -0800

    PEP8 Validation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """ Rectangle Class that inherits from Base """
 from models.base import Base
 
+
 class Rectangle(Base):
     """ Rectangle Class """
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -14,7 +15,7 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-    
+ 
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -26,7 +27,7 @@ class Rectangle(Base):
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
@@ -34,21 +35,21 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-    
+
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if value < 0:
             raise ValueError("x must be > 0")
         self.__x = value
-    
+
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         if value < 0:
